b673b7e17d3f156a5211ef65431b991168bbf831
breadtf <bread@mail.breadtf.com>
2024-01-23 15:18:24 +0000

    Minify index.php

--- index.php
+++ index.php
@@ -1,23 +1,13 @@
 <?php
 
-#require_once('vendor/autoload.php');
-
-# uhhh sure lets do this instead cuz above wont work
-# thanks php
 require_once("src/uwurandom.php");
-
 $uwurandom = new Breadtf\Uwurandom\uwurandom();
 
-
-
 $uwu = $uwurandom -> generate(500);
-
 echo "\n\n" . $uwu . "\n\n";
 echo strlen($uwu);
 
-
 $uwusoftmax = $uwurandom -> generate(500, true);
-
 echo "\n\n" . $uwusoftmax . "\n\n";
 echo strlen($uwusoftmax);
 ?>
